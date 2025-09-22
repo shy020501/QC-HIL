00b877d664d253f5cab6f906ee386e8ab0f5be55
Kim Juneseo <99635136+kimhedol00@users.noreply.github.com>
2025-09-22 13:56:51 +0900

    Add files via upload

---
+++ train.sh
@@ -0,0 +1,14 @@
+export XLA_FLAGS=--xla_gpu_autotune_level=0
+export JAX_DEFAULT_matmul_precision='float32'
+export XLA_PYTHON_CLIENT_PREALLOCATE=false 
+
+MUJOCO_GL=egl python main.py \
+  --run_group=MY_ROBOT \
+  --env_name=AGI_TF \
+  --sparse=True \
+  --horizon_length=10 \
+  --eval_interval 0 \
+  --custom_dataset_path ../data_preprocessing/final_robotics_dataset.h5 \
+  --save_interval 10000 \
+  --online_steps 100000 \
+  --offline_steps 100000 \
